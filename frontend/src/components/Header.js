e3ddc25603dadcc14ad9fd6c4d306578876864ea
newton <kamaunewton78@gmail.com>
2021-03-26 13:24:24 +0300

    added navbar

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,25 @@
-
+import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
-    return (
-        <header>
-         Header   
-        </header>
-    )
-}
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container>
+          <Navbar.Brand href="/">Duka Yangu</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="/cart">
+                <i className="fa fa-shopping-cart" aria-hidden="true"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i class="fa fa-user" aria-hidden="true"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
