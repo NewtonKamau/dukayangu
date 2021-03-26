b76c266d7cf0af48ba06e717da15beebcadf1bf0
newton <kamaunewton78@gmail.com>
2021-03-26 15:07:05 +0300

    rating component with the stars

---
+++ frontend/src/components/Ratings.js
@@ -0,0 +1,60 @@
+const Ratings = ({ value, text }) => {
+  return (
+    <div className="rating">
+      <span>
+        <i
+          className={
+            value >= 1
+              ? "fas fa-star"
+              : (value) =>
+                  0.5 ? "fas fa-star-half-alt" : (value = '"fas fa-star"')
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 2
+              ? "fas fa-star"
+              : (value) =>
+                  1.5 ? "fas fa-star-half-alt" : (value = '"fas fa-star"')
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 3
+              ? "fas fa-star"
+              : (value) =>
+                  2.5 ? "fas fa-star-half-alt" : (value = '"fas fa-star"')
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 4
+              ? "fas fa-star"
+              : (value) =>
+                  3.5 ? "fas fa-star-half-alt" : (value = '"fas fa-star"')
+          }
+        ></i>
+      </span>
+      <span>
+        <i
+          className={
+            value >= 5
+              ? "fas fa-star"
+              : (value) =>
+                  4.5 ? "fas fa-star-half-alt" : (value = '"fas fa-star"')
+          }
+              ></i>
+             
+          </span>
+          <span>{text && text}</span>
+    </div>
+  );
+};
+
+export default Ratings;
