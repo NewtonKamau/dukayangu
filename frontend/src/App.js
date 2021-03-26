579c86790e2d7b4662da93a817eab83e69558f7d
newton <kamaunewton78@gmail.com>
2021-03-26 12:23:10 +0300

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+
+const App = ()=> {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Duka Yangu Online</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
