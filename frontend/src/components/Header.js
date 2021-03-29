58d799e3417a87ee0fb8706a7700e5d5462f6b80
newton <kamaunewton78@gmail.com>
2021-03-29 14:21:37 +0300

    implement router to the cart and login

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,19 +1,26 @@
+import { LinkContainer } from 'react-router-bootstrap'
 import { Navbar, Nav, Container } from "react-bootstrap";
 const Header = () => {
   return (
     <header>
       <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-          <Navbar.Brand href="/">Duka Yangu</Navbar.Brand>
+          <LinkContainer to="/">
+            <Navbar.Brand>Duka Yangu</Navbar.Brand>
+          </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link href="/cart">
-                <i className="fa fa-shopping-cart" aria-hidden="true"></i>Cart
-              </Nav.Link>
-              <Nav.Link href="/login">
-                <i class="fa fa-user" aria-hidden="true"></i>Login
-              </Nav.Link>
+              <LinkContainer to="/cart">
+                <Nav.Link>
+                  <i className="fa fa-shopping-cart" aria-hidden="true"></i>Cart
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/login">
+                <Nav.Link >
+                  <i class="fa fa-user" aria-hidden="true"></i>Login
+                </Nav.Link>
+              </LinkContainer>
             </Nav>
           </Navbar.Collapse>
         </Container>
