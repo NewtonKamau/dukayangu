c6c4899711062af04b5fcf12697cc2df8fc9c1e4
newton <kamaunewton78@gmail.com>
2021-03-30 14:52:48 +0300

    export products and import on the server, show all products from api and also show a product by id

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express');
-
+const products = require("./data/products")
 const app = new express();
 
-app.get("/",(req, res) =>{
-    res.send("api is running")
+app.get("/api/products", (req, res) => {
+    res.json(products)
 });
-
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((p) => p._id === req.params.id)
+    res.json(product);
+});
 
 app.listen(5000,console.log("Server running on port 5000"))
