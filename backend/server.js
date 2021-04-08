f047e1df3997eef7527d20af5cce8694d7e82619
NewtonKamau <kamaunewton78@gmail.com>
2021-04-08 13:00:29 +0300

    ecma 6 features

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,11 @@
-const express = require('express');
-const products = require("./data/products")
+import express from "express"
+import dotenv from "dotenv"
+import  products from "./data/products"
+
+dotenv.config();
 const app = new express();
+
+
 
 app.get("/api/products", (req, res) => {
     res.json(products)
@@ -10,4 +15,6 @@ app.get("/api/products/:id", (req, res) => {
     res.json(product);
 });
 
-app.listen(5000,console.log("Server running on port 5000"))
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT,console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
