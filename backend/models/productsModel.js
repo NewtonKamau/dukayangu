f385256a4a01a0e2c640bb4cfbfb318df1b5dbd6
NewtonKamau <kamaunewton78@gmail.com>
2021-04-08 15:15:54 +0300

    models

---
+++ backend/models/productsModel.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const reviewSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  rating: { type: Number, required: true },
+  comment: { type: String, required: true },
+}, {
+    timestamps:true
+});
+
+const productSchema = mongoose.Schema(
+  {
+    // add relationship between product and user
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: number,
+      required: true,
+      default: 0,
+        },
+    review:[reviewSchema],
+    numReviews: {
+      type: String,
+      required: true,
+      default: 0,
+    },
+    price: {
+      type: String,
+      required: true,
+      default: 0,
+    },
+    countInStock: {
+      type: String,
+      required: true,
+      default: 0,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const Product = mongoose.model("Product", productSchema);
+export default Product;
