958bd97e47a6528ec7b6226692733b587a9ba98c
NewtonKamau <kamaunewton78@gmail.com>
2021-04-19 17:41:07 +0300

    products routes

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import asyncHandler from 'express-async-handler';
+const router = express.Router();
+import Product from '../models/productsModel.js';
+
+//@desc fetch all products
+//@route GET api/products
+//access Public
+
+router.get("/",asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+  res.json(products);
+}));
+
+//@desc fetch product by id
+//access public 
+router.get("/:id",asyncHandler (async(req, res) => {
+    const product = await Product.findById(req.params.id)
+    if (product) {
+      res.json(product);
+    }else {
+        res.status(404).json({message: "Product Not Found"});
+  }
+  
+}));
+
+export default router;
