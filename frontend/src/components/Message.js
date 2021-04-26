d4ac5f41e1e11bfcac62bd00443796b87f7c7c25
NewtonKamau <kamaunewton78@gmail.com>
2021-04-26 05:24:46 +0300

    refactored

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -8,7 +8,7 @@ const Message = ({variant , children}) => {
     ) 
 }
 Message.defaultProps = {
-    variant = 'info',
+    variant : 'info',
 }
 
 export default Message
--- frontend/src/reducers/productsReducers.js
+++ frontend/src/reducers/productsReducers.js
@@ -1,5 +1,8 @@
  
-import {PRODUCT_LIST_REQUEST,PRODUCT_LIST_SUCCESS,PRODUCT_LIST_FAIL} from '../constants/productsContstants';
+import {
+  PRODUCT_LIST_REQUEST, PRODUCT_LIST_SUCCESS, PRODUCT_LIST_FAIL,
+  PRODUCT_DETAILS_REQUEST, PRODUCT_DETAILS_SUCCESS, PRODUCT_DETAILS_FAIL
+} from '../constants/productsContstants';
 export const productListReducer = (state = { products: [] }, action) => {
   switch (action.type) {
     case PRODUCT_LIST_REQUEST:
@@ -11,4 +14,17 @@ export const productListReducer = (state = { products: [] }, action) => {
     default:
       return state;
   }
-};
+  
+}
+export const producDetailsReducer = (state = { product: { reviews:[]} }, action) => {
+  switch (action.type) {
+    case PRODUCT_DETAILS_REQUEST:
+      return { loading: true, ...state };
+    case PRODUCT_DETAILS_SUCCESS:
+      return { loading: false, product: action.payload };
+    case PRODUCT_DETAILS_FAIL:
+      return { loading: false, error: action.payload };
+    default:
+      return state;
+  }
+}
