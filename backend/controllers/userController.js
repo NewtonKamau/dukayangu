4f0ecbdad55a7d08026be6da5e23d223571ebec7
NewtonKamau <kamaunewton78@gmail.com>
2021-04-26 17:44:22 +0300

    jwtwebtoken for authorization

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,5 @@
 import asyncHandler from "express-async-handler";
-
+import generateToken from '../utils/generateTokens.js';
 import User from "../models/userModel.js";
 
 //@desc fetch all auth users
@@ -10,16 +10,17 @@ const getAuthUsers = asyncHandler(async (req, res) => {
     const user = await User.findOne({ email })
     if (user && (await user.matchPassword(password) )) {
         res.json({
-            _id: user.id,
+            _id: user._id,
             name: user.name,
             email:user.email,
             password: user.password,
             isAdmin: user.isAdmin,
-            token:null
+            token: generateToken(user._id),
         })
     } else {
         res.status(401)
         throw new Error("Invalid  email or password");
+     
     }
 });
 
