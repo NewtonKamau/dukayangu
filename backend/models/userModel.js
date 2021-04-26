4f165cf111d193a2cf2d3fef2110b7e070b856e5
NewtonKamau <kamaunewton78@gmail.com>
2021-04-26 14:16:03 +0300

    check matched passwords else throw error

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-
+import bcrypt from 'bcryptjs';
 const userSchema = mongoose.Schema(
   {
     name: {
@@ -25,5 +25,10 @@ const userSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+//create a method to encrypt the password
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
 const User = mongoose.model("User", userSchema);
 export default User;
