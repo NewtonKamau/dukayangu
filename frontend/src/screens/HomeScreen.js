257f6563d4fa7e7f6963e6c67431c77cc962725c
NewtonKamau <kamaunewton78@gmail.com>
2021-04-26 04:43:48 +0300

    bring redux to homeScreen and removed useState ,implemented useSelector,useDispatch and loading the data using dispact()

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,30 +1,37 @@
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
+import {useDispatch, useSelector} from 'react-redux';
 import { Row, Col } from "react-bootstrap"
-import axios from "axios"
+import {listProducts} from '../actions/productionActions';
+
 import Product from "../components/Product"
-
+//definde dispatch 
 const HomeScreen = () => {
-  const [products, setProducts] = useState([])
-
+  const dispatch = useDispatch();
+//bring in products from the store 
+  const productList = useSelector(state => state.productList)
+  //pull loading, error and products from the state
+  const {loading, error,products } = productList
   useEffect(() => {
-    //fetch data from the api using axios in asychronous manner
-    const fetchProducts = async () => {
-      const { data } = await axios.get("/api/products");
-      //set our products to show other than an empty array[]
-      setProducts(data);
-    };
-    fetchProducts();
-  }, []);
+   //use dispatch to get the products instead of axios
+    dispatch(listProducts())
+  }, [dispatch])
+  
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
+      
+      {loading ? (<h2>Loading....</h2>) : error ? (
+        <h3>{error}</h3>
+      ) : (
+         <Row>
         {products.map((product) => (
           <Col sm={12} md={6} lg={4} xl={3}>
             <Product product={product} />
           </Col>
         ))}
-      </Row>
+      </Row> 
+      )}
+      
     </>
   );
 };
