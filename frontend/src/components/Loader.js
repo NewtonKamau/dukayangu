c2d6c25ba8c5b63d5e25eabe5a72982faa525abd
NewtonKamau <kamaunewton78@gmail.com>
2021-04-26 05:26:18 +0300

    refactored

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -4,10 +4,10 @@ const Loader = () => {
     return <Spinner animation="border"
         role='status'
         style = {{
-            width = '100px',
-            height = '100px',
-            margin = 'auto',
-            display = 'block'
+            width : '100px',
+            height : '100px',
+            margin : 'auto',
+            display : 'block'
         }}
     >
         <span className='sr-only'>Loading .... </span>
