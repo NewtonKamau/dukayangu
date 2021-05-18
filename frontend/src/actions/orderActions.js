a819cf6a67fa337506fdbbd4d2c5cadda2664f05
NewtonKamau <kamaunewton78@gmail.com>
2021-05-18 07:50:32 +0300

    redirect if no orders in the cart or shipping details

--- frontend/src/actions/orderActions.js
+++ frontend/src/actions/orderActions.js
@@ -17,89 +17,113 @@ export const createOrder = (order) => async (dispatch, getState) => {
     dispatch({
       type: ORDER_CREATE_REQUEST,
     });
-    //destructure user details from the login state so that we get the token
+
     const {
       userLogin: { userInfo },
     } = getState();
+
     const config = {
-      Headers: {
-        "content-type": "application/json",
+      headers: {
+        "Content-Type": "application/json",
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
+
     const { data } = await axios.post(`/api/orders`, order, config);
+
     dispatch({
       type: ORDER_CREATE_SUCCESS,
       payload: data,
     });
+    
+    localStorage.removeItem("cartItems");
   } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+   
     dispatch({
       type: ORDER_CREATE_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
+      payload: message,
     });
   }
 };
 
-export const payOrder = (orderId,paymentResult) => async (dispatch, getState) => {
+
+export const payOrder =
+  (orderId, paymentResult) => async (dispatch, getState) => {
+    try {
+      dispatch({
+        type: ORDER_PAY_REQUEST,
+      });
+
+      const {
+        userLogin: { userInfo },
+      } = getState();
+
+      const config = {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${userInfo.token}`,
+        },
+      };
+
+      const { data } = await axios.put(
+        `/api/orders/${orderId}/pay`,
+        paymentResult,
+        config
+      );
+
+      dispatch({
+        type: ORDER_PAY_SUCCESS,
+        payload: data,
+      });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      
+      dispatch({
+        type: ORDER_PAY_FAIL,
+        payload: message,
+      });
+    }
+  };
+export const getOrderDetails = (id) => async (dispatch, getState) => {
   try {
     dispatch({
       type: ORDER_DETAILS_REQUEST,
     });
-    //destructure user details from the login state so that we get the token
+
     const {
       userLogin: { userInfo },
     } = getState();
+
     const config = {
-      Headers: {
-     
+      headers: {
         Authorization: `Bearer ${userInfo.token}`,
       },
     };
-    const { data } = await axios.get(`/api/orders/${orderId}`, config);
+
+    const { data } = await axios.get(`/api/orders/${id}`, config);
+
     dispatch({
       type: ORDER_DETAILS_SUCCESS,
       payload: data,
     });
   } catch (error) {
+    const message =
+      error.response && error.response.data.message
+        ? error.response.data.message
+        : error.message;
+    if (message === "Not authorized, token failed") {
+      dispatch(logout());
+    }
     dispatch({
       type: ORDER_DETAILS_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
-    });
-  }
-};
-export const getOrderDetails = (orderId, paymentResult) => async (dispatch, getState) => {
-  try {
-    dispatch({
-      type: ORDER_PAY_REQUEST,
-    });
-    //destructure user details from the login state so that we get the token
-    const {
-      userLogin: { userInfo },
-    } = getState();
-    const config = {
-      Headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${userInfo.token}`,
-      },
-    };
-    const { data } = await axios.put(`/api/orders/${orderId}/pay`,paymentResult, config);
-    dispatch({
-      type: ORDER_PAY_SUCCESS,
-      payload: data,
-    });
-  } catch (error) {
-    dispatch({
-      type: ORDER_PAY_FAIL,
-      payload:
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message,
+      payload: message,
     });
   }
 };
--- frontend/src/screens/PlaceOrderScreen.js
+++ frontend/src/screens/PlaceOrderScreen.js
@@ -1,26 +1,21 @@
-import {
-  Button,
-  Col,
-  Row,
-  ListGroup,
-  Image,
-  Card,
-  ListGroupItem,
-} from "react-bootstrap";
+import {useEffect} from 'react';
 import { Link } from "react-router-dom";
-import { useEffect } from "react";
+import { Button, Row, Col, ListGroup, Image, Card } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
 import Message from "../components/Message";
-import Loader from "../components/Loader";
-
 import CheckoutSteps from "../components/CheckoutSteps";
-import {createOrder} from "../actions/orderActions";
+import { createOrder } from "../actions/orderActions";
 
-const PlaceOrderScreen = ({history}) => {
+const PlaceOrderScreen = ({ history }) => {
   const dispatch = useDispatch();
 
   const cart = useSelector((state) => state.cart);
-  
+
+  if (!cart.shippingAddress.address) {
+    history.push("/shipping");
+  } else if (!cart.paymentMethod) {
+    history.push("/payment");
+  }
   //   Calculate prices
   const addDecimals = (num) => {
     return (Math.round(num * 100) / 100).toFixed(2);
@@ -37,60 +32,59 @@ const PlaceOrderScreen = ({history}) => {
     Number(cart.taxPrice)
   ).toFixed(2);
 
-  const orderCreate = useSelector(state => state.orderCreate);
-  const {loading, order, success, error } = orderCreate;
+  const orderCreate = useSelector((state) => state.orderCreate);
+  const { order, success, error } = orderCreate;
 
   useEffect(() => {
     if (success) {
-      history.push(`/orders/${order._id}`);
+      history.push(`/order/${order._id}`);
     }
-
     // eslint-disable-next-line
   }, [history, success]);
+
   const placeOrderHandler = () => {
     dispatch(
       createOrder({
-        orderItems: cart.cartItems,
+        orderItems: cart.cartItems,        shippingAddress: cart.shippingAddress,
         paymentMethod: cart.paymentMethod,
-        shippingAddress: cart.shippingAddress,
-        shippingPrice: cart.shippingPrice,
         itemsPrice: cart.itemsPrice,
-        taxPrice: cart.itemsPrice,
+        shippingPrice: cart.shippingPrice,
+        taxPrice: cart.taxPrice,
         totalPrice: cart.totalPrice,
       })
     );
   };
+
   return (
     <>
       <CheckoutSteps step1 step2 step3 step4 />
-      {loading  && <Loader />}
       <Row>
         <Col md={8}>
           <ListGroup variant="flush">
-            <ListGroupItem>
+            <ListGroup.Item>
               <h2>Shipping</h2>
               <p>
                 <strong>Address:</strong>
-                {cart.shippingAddress.address},{cart.shippingAddress.city},
-                {cart.shippingAddress.postalCode},{cart.shippingAddress.country}
+                {cart.shippingAddress.address}, {cart.shippingAddress.city}{" "}
+                {cart.shippingAddress.postalCode},{" "}
+                {cart.shippingAddress.country}
               </p>
-            </ListGroupItem>
+            </ListGroup.Item>
 
-            <ListGroupItem>
+            <ListGroup.Item>
               <h2>Payment Method</h2>
-              <p>
-                <strong>Method:</strong>
-                {cart.paymentMethod}
-              </p>
-            </ListGroupItem>
-            <ListGroupItem>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
               <h2>Order Items</h2>
               {cart.cartItems.length === 0 ? (
                 <Message>Your cart is empty</Message>
               ) : (
                 <ListGroup variant="flush">
                   {cart.cartItems.map((item, index) => (
-                    <ListGroupItem key={index}>
+                    <ListGroup.Item key={index}>
                       <Row>
                         <Col md={1}>
                           <Image
@@ -109,47 +103,47 @@ const PlaceOrderScreen = ({history}) => {
                           {item.qty} x ${item.price} = ${item.qty * item.price}
                         </Col>
                       </Row>
-                    </ListGroupItem>
+                    </ListGroup.Item>
                   ))}
                 </ListGroup>
               )}
-            </ListGroupItem>
+            </ListGroup.Item>
           </ListGroup>
         </Col>
         <Col md={4}>
           <Card>
-            <ListGroup>
-              <ListGroupItem>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
                 <h2>Order Summary</h2>
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 <Row>
                   <Col>Items</Col>
                   <Col>${cart.itemsPrice}</Col>
                 </Row>
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 <Row>
                   <Col>Shipping</Col>
                   <Col>${cart.shippingPrice}</Col>
                 </Row>
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 <Row>
                   <Col>Tax</Col>
                   <Col>${cart.taxPrice}</Col>
                 </Row>
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 <Row>
                   <Col>Total</Col>
                   <Col>${cart.totalPrice}</Col>
                 </Row>
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 {error && <Message variant="danger">{error}</Message>}
-              </ListGroupItem>
-              <ListGroupItem>
+              </ListGroup.Item>
+              <ListGroup.Item>
                 <Button
                   type="button"
                   className="btn-block"
@@ -158,7 +152,7 @@ const PlaceOrderScreen = ({history}) => {
                 >
                   Place Order
                 </Button>
-              </ListGroupItem>
+              </ListGroup.Item>
             </ListGroup>
           </Card>
         </Col>
@@ -166,5 +160,4 @@ const PlaceOrderScreen = ({history}) => {
     </>
   );
 };
-
 export default PlaceOrderScreen;
