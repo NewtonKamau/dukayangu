ff6d8f58638fb6e3f529d600e5f07fad81e79727
NewtonKamau <kamaunewton78@gmail.com>
2021-06-14 10:04:04 +0300

    create review backend

--- backend/models/productsModel.js
+++ backend/models/productsModel.js
@@ -4,6 +4,12 @@ const reviewSchema = mongoose.Schema({
   name: { type: String, required: true },
   rating: { type: Number, required: true },
   comment: { type: String, required: true },
+  // add relationship between comment/review and user
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
 }, {
     timestamps:true
 });
--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from 'express';
-import { deleteProduct, getProductById, getProducts,updateProduct,createProduct } from '../controllers/productController.js';
+import { deleteProduct, getProductById, getProducts,updateProduct,createProduct,createProductReview } from '../controllers/productController.js';
 const router = express.Router();
 import { protect, admin } from "../middleware/authMiddleware.js";
 
 
 router.route("/").get(getProducts).post(protect,createProduct);
+router.route("/:id/reviews").post(protect,createProductReview);
 
 router.route("/:id").get(getProductById).delete(protect, admin, deleteProduct).put(protect,updateProduct);
 
